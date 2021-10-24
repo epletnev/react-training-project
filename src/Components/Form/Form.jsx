2651e58bcda06bd1ca68a7dd8496ebc70204078a
Evgeny Pletnev <e.a.pletnev@gmail.com>
2021-10-24 17:15:57 +0100

    Updated user form with city and country fields and created event handler functions for state change

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -3,11 +3,20 @@ import React, { Component } from "react";
 class Form extends React.Component {
   constructor() {
     super();
-    this.state = { name: "", email: "", phone: "", state: "" };
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      state: "",
+      city: "",
+      country: "",
+    };
     this.nameChange = this.nameChange.bind(this);
     this.emailChange = this.emailChange.bind(this);
     this.phoneChange = this.phoneChange.bind(this);
     this.stateChange = this.stateChange.bind(this);
+    this.cityChange = this.cityChange.bind(this);
+    this.countryChange = this.countryChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
@@ -27,12 +36,22 @@ class Form extends React.Component {
     this.setState({ state: e.target.value });
   }
 
+  cityChange(e) {
+    this.setState({ city: e.target.value });
+  }
+
+  countryChange(e) {
+    this.setState({ country: e.target.value });
+  }
+
   onSubmit(e) {
     e.preventDefault();
     console.log(this.state.name);
     console.log(this.state.email);
     console.log(this.state.phone);
     console.log(this.state.state);
+    console.log(this.state.city);
+    console.log(this.state.country);
   }
 
   render() {
@@ -75,11 +94,23 @@ class Form extends React.Component {
             onChange={this.stateChange}
           />
 
-          {/* <label for="city">CITY:</label>
-          <input type="text" id="city" name="city" value="City" />
+          <label for="city">CITY:</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={this.state.city}
+            onChange={this.cityChange}
+          />
 
           <label for="country">COUNTRY:</label>
-          <input type="text" id="country" name="country" value="Country" /> */}
+          <input
+            type="text"
+            id="country"
+            name="country"
+            value={this.state.country}
+            onChange={this.countryChange}
+          />
           <input type="submit" value="Register" />
         </form>
       </div>
